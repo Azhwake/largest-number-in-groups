47ac446ea06b4730e0c57e29fd18e8f05e69ee0e
Azhwake <azhwaq2002@gmail.com>
2024-09-21 22:44:55 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // Good Luck 💪🏾
+function findLargestNumbers(arr) {
+    return arr.map(subArray => Math.max(...subArray));  // Find max in each sub-array
+}
+
+// Test cases
+console.log(findLargestNumbers([[7, 3, 8, 11], [54, 12, 43, 77], [16, 11, 8]])); // ➞ [11, 77, 16]
+console.log(findLargestNumbers([[22, 55, 12], [45, 66, 95], [22, 5, 11]]));     // ➞ [55, 95, 22]
